beac648f8b463b6242f6c39872fcf2d52ba1c70f
alisonege <aegarrettengele@gmail.com>
2018-12-04 21:42:29 -0600

    average times now

--- src/TreapsvsAVL.java
+++ src/TreapsvsAVL.java
@@ -10,77 +10,97 @@ public class TreapsvsAVL {
 
         System.out.println("Testing treap insertion\n ---------------------------");
 
-        TreapNode treapRoot = new TreapNode();
-        treapRoot = null;
-
+        TreapNode treapRoot = null;
+        double avgTimes = 0;
         for (ArrayList arr : testArrays) { //for each arraylist in test array, println each one
             double start = System.currentTimeMillis();
-            for (int i = 0; i < arr.size(); i++){
-                treapRoot = treapRoot.insert(treapRoot, (int) arr.get(i));
+            for (int i = 0; i<10; i++) {
+                for (int j = 0; j < arr.size(); j++) {
+                    treapRoot = treapRoot.insert(treapRoot, (int) arr.get(j));
+                }
             }
             double end = System.currentTimeMillis();
             double totalTime = end - start;
-            System.out.println("It took: " + (long) totalTime + " milliseconds to insert n= " + arr.size() + " values into a treap");
+            avgTimes = totalTime/10;
+            System.out.println("It took an average of : " + (long) avgTimes + " milliseconds to insert n= " + arr.size() + " values into a treap");
         }
+
+
         System.out.println("---------------------------");
         System.out.println("Testing AVL insertion\n ---------------------------");
         AVL avlTree = new AVL();
+
         for (ArrayList arr : testArrays) { //for each arraylist in test array, println each one
             double start = System.currentTimeMillis();
-            for (int i = 0; i < arr.size(); i++){
-                avlTree.root = avlTree.insert(avlTree.root, (int) arr.get(i));
+            for (int i = 0; i<10; i++) {
+                for (int j = 0; j < arr.size(); j++) {
+                    avlTree.root = avlTree.insert(avlTree.root, (int) arr.get(j));
+                }
             }
             double end = System.currentTimeMillis();
             double totalTime = end - start;
-            System.out.println("It took: " + (long) totalTime + " milliseconds to insert n= " + arr.size() + " values into an AVL tree");
+            avgTimes = totalTime/10;
+            System.out.println("It took an average of: " + (long) avgTimes + " milliseconds to insert n= " + arr.size() + " values into an AVL tree");
         }
         System.out.println("---------------------------");
         System.out.println("Testing treap search\n ---------------------------");
 
         for (ArrayList arr : testArrays) { //for each arraylist in test array, println each one
             double start = System.currentTimeMillis();
-            for(int i = 0; i <= arr.size(); i++){
-                int chosenIndex = (int) (Math.random() * (double) arr.size());
-                treapRoot.search(treapRoot, chosenIndex);
+            for (int i = 0; i<10; i++) {
+                for (int j = 0; j < arr.size(); j++) {
+                    int chosenIndex = (int) (Math.random() * (double) arr.size());
+                    treapRoot.search(treapRoot, chosenIndex);
+                }
             }
             double end = System.currentTimeMillis();
             double totalTime = end - start;
-            System.out.println("It took: " + (long) totalTime + " milliseconds to search for n= " + arr.size() + " values in a treap of size n");
+            avgTimes = totalTime/10;
+            System.out.println("It took an average of: " + (long) avgTimes + " milliseconds to search for n= " + arr.size() + " values in a treap of size n");
         }
         System.out.println("---------------------------");
         System.out.println("Testing AVL search\n ---------------------------");
 
         for (ArrayList arr : testArrays) { //for each arraylist in test array, println each one
             double start = System.currentTimeMillis();
-            for(int i = 0; i <= arr.size(); i++){
-                int chosenIndex = (int) (Math.random() * (double) arr.size());
-                avlTree.search(avlTree.root, chosenIndex);
+            for (int i = 0; i<10; i++) {
+                for (int j = 0; j < arr.size(); j++) {
+                    int chosenIndex = (int) (Math.random() * (double) arr.size());
+                    avlTree.search(avlTree.root, chosenIndex);
+                }
             }
             double end = System.currentTimeMillis();
             double totalTime = end - start;
-            System.out.println("It took: " + (long) totalTime + " milliseconds to search for n= " + arr.size() + " values in an AVL tree of size n");
+            avgTimes = totalTime/10;
+            System.out.println("It took an average of : " + (long) avgTimes + " milliseconds to search for n= " + arr.size() + " values in an AVL tree of size n");
         }
         System.out.println("---------------------------");
         System.out.println("Testing Treap deletion\n ---------------------------");
         for (ArrayList arr : testArrays) { //for each arraylist in test array, println each one
             double start = System.currentTimeMillis();
-            for (int i = 0; i < arr.size(); i++){
-                treapRoot = treapRoot.deleteNode(treapRoot, (int) arr.get(i));
+            for (int i = 0; i<10; i++) {
+                for (int j = 0; j < arr.size(); j++) {
+                    treapRoot = treapRoot.deleteNode(treapRoot, (int) arr.get(j));
+                }
             }
             double end = System.currentTimeMillis();
             double totalTime = end - start;
-            System.out.println("It took: " + (long) totalTime + " milliseconds to delete n= " + arr.size() + " values from a treap");
+            avgTimes = totalTime/10;
+            System.out.println("It took an average of : " + (long) avgTimes + " milliseconds to delete n= " + arr.size() + " values from a treap");
         }
         System.out.println("---------------------------");
         System.out.println("Testing AVL deletion\n ---------------------------");
         for (ArrayList arr : testArrays) { //for each arraylist in test array, println each one
             double start = System.currentTimeMillis();
-            for (int i = 0; i < arr.size(); i++){
-                avlTree.root = avlTree.deleteNode(avlTree.root, (int) arr.get(i));
+            for (int i = 0; i<10; i++) {
+                for (int j = 0; j < arr.size(); j++) {
+                    avlTree.root = avlTree.deleteNode(avlTree.root, (int) arr.get(j));
+                }
             }
             double end = System.currentTimeMillis();
             double totalTime = end - start;
-            System.out.println("It took: " + (long) totalTime + " milliseconds to delete n= " + arr.size() + " values from an AVL tree");
+            avgTimes = totalTime/10;
+            System.out.println("It took an average of : " + (long) avgTimes + " milliseconds to delete n= " + arr.size() + " values from an AVL tree");
         }
         System.out.println("---------------------------");
 
